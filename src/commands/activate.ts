ed8549790203837f2360037766e0b4270db579cc
supermeap123 <garrentvanmeter@gmail.com>
2025-02-10 19:32:46 -0800

    feat: Add isActive property to ExtendedClient and update activate command

--- src/commands/activate.ts
+++ src/commands/activate.ts
@@ -1,9 +1,12 @@
 import { SlashCommandBuilder } from 'discord.js';
+import { ExtendedClient } from '../utils/ExtendedClient';
 
 export const data = new SlashCommandBuilder()
   .setName('activate')
   .setDescription('Activates the bot.');
 
 export const execute = async (interaction: any) => {
+  const client = interaction.client as ExtendedClient;
+  client.isActive = true;
   await interaction.reply('Bot activated!');
 };
--- src/utils/ExtendedClient.ts
+++ src/utils/ExtendedClient.ts
@@ -2,9 +2,11 @@ import { Client, Collection } from 'discord.js';
 
 export class ExtendedClient extends Client {
   commands: Collection<string, any>;
+  isActive: boolean;
 
   constructor(options: any) {
     super(options);
     this.commands = new Collection();
+    this.isActive = false;
   }
 }
