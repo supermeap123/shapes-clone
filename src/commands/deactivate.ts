00a42dee012064632f47ecb7e2c3361f5a73d813
supermeap123 <garrentvanmeter@gmail.com>
2025-02-10 19:33:12 -0800

    feat: Update deactivate command

--- src/commands/deactivate.ts
+++ src/commands/deactivate.ts
@@ -1,9 +1,12 @@
 import { SlashCommandBuilder } from 'discord.js';
+import { ExtendedClient } from '../utils/ExtendedClient';
 
 export const data = new SlashCommandBuilder()
   .setName('deactivate')
   .setDescription('Deactivates the bot.');
 
 export const execute = async (interaction: any) => {
+  const client = interaction.client as ExtendedClient;
+  client.isActive = false;
   await interaction.reply('Bot deactivated!');
 };
